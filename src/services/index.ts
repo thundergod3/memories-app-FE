d4c34036176ebeeb471cc28bf360e64122151eb4
Phan Huy Công <43270933+thundergod3@users.noreply.github.com>
2021-01-29 11:10:19 +0700

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -7,7 +7,7 @@ class HTTPMethod {
 
 	constructor() {
 		this.axios = axios;
-		this.axios.defaults.baseURL = "http://localhost:8080";
+		this.axios.defaults.baseURL = "https://memories-website.herokuapp.com/";
 	}
 
 	get = (url: string, remainProps?: any): Promise<any> => axios.get(url, remainProps);
